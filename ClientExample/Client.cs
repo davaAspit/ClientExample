621fe75f625368b28aaf43bc858a70937af8cd3d
Daniel Valsby-Koch <dava@aspit.dk>
2018-09-12 11:43:40 +0200

    Add project files.

---
+++ ClientExample/Client.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientExample
+{
+    public class Client : IDisposable
+    {
+        /// <summary>
+        /// Used to store the server connection
+        /// </summary>
+        public TcpClient ServerConnection { get; }
+
+        /// <summary>
+        /// Initializes the server connection with the the hostname and port
+        /// </summary>
+        /// <param name="hostname">Hostname of the server. Use localhost if internal server</param>
+        /// <param name="port">The port of the server</param>
+        public Client(string hostname, int port)
+        {
+            ServerConnection = new TcpClient(hostname, port);
+        }
+
+        /// <summary>
+        /// Used to send a request to the server.
+        /// </summary>
+        /// <param name="request">The request to the server</param>
+        /// <returns>A boolean defining whether it went good or bad</returns>
+        public bool SendLine(string request)
+        {
+            try
+            {
+                using (NetworkStream ns = ServerConnection.GetStream())
+                using (StreamWriter writer = new StreamWriter(ns))
+                {
+                    writer.AutoFlush = true;
+
+                    writer.WriteLine(request);
+                }
+            }
+            catch (IOException)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Reads a line from the server.
+        /// </summary>
+        /// <param name="response">The response from the server</param>
+        /// <returns>A boolean defining whether it went good or bad</returns>
+        public bool ReceiveLine(out string response)
+        {
+            response = "";
+            try
+            {
+                using (NetworkStream ns = ServerConnection.GetStream())
+                using (StreamReader reader = new StreamReader(ns))
+                {
+                    response = reader.ReadLine();
+                }
+            }
+            catch (IOException)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public void Dispose()
+        {
+            ServerConnection.Dispose();
+        }
+    }
+}
---
+++ ClientExample/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //This is possible because Client is IDisposable
+            using (Client client = new Client("localhost", 9999))
+            {
+
+                //Take input from console and send
+                //If input is Exit - stop it
+                while (true)
+                {
+                    Console.WriteLine("Skriv din forespørgsel til serveren. Skriv exit for at stoppe programmet.");
+                    string request = Console.ReadLine();
+                    if (request.ToLower() == "exit")
+                        break;
+                    client.SendLine(request);
+
+                    if (client.ReceiveLine(out string response))
+                    {
+                        Console.WriteLine("Svar fra server: " + response);
+                    }
+                }
+            }
+
+        }
+    }
+}
